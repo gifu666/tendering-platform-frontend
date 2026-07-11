20572ddf6a2581ebe0f6479d861a542162303b58
gifu666 <3059654170@qq.com>
2026-07-11 20:54:39 +0800

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({plugins:[react()],server:{port:5173,host:'0.0.0.0'}});
+
+export default defineConfig({
+  base: '/tendering-platform-frontend/',
+  plugins: [react()],
+  server: { port: 5173, host: '0.0.0.0' },
+});
